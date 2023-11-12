d07967ae33e849e39f60c51ed62fe5707d9a2f10
Daniel Garcia Smester <danielgarcia3681@gmail.com>
2023-11-12 01:01:49 -0500

    Merge remote-tracking branch 'refs/remotes/origin/daniel-timer-score'

--- TableTopAssistant/Timer/TimerViewController.swift
+++ TableTopAssistant/Timer/TimerViewController.swift
@@ -12,21 +12,41 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var timeTextView: UITextView!
+    
+    var seconds = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+        
+    @IBAction func startTimer(_ sender: UIButton) {
+        
+        guard let timerInfo = timeTextView.text else { return }
+        
+        do {
+            seconds = try timeToSeconds(for: timerInfo)
+        } catch {
+            print("Uh-oh!")
+        }
+        
+        guard seconds > 0 else { return }
+        
+        timeTextView.isEditable = false
+        resetButton.isHidden = false
+        
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(count), userInfo: nil, repeats: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func resetTimer(_ sender: UIButton) {
     }
-    */
-
+    
+    @objc func count() {
+        seconds -= 1
+        timeTextView.text = secondsToTime(for: seconds)
+    }
+    
 }
+
