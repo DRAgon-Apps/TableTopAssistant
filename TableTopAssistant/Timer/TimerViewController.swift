48ab3a2b938b9b633fe0ddf2512a8598e93e3c1d
Daniel Garcia Smester <danielgarcia3681@gmail.com>
2023-11-12 02:05:30 -0500

    Merge remote-tracking branch 'refs/remotes/origin/daniel-timer-score'

--- TableTopAssistant/Timer/TimerViewController.swift
+++ TableTopAssistant/Timer/TimerViewController.swift
@@ -16,6 +16,8 @@ class TimerViewController: UIViewController {
     var seconds = 0
     var timer = Timer()
     
+    var hasStarted = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,21 +25,41 @@ class TimerViewController: UIViewController {
         
     @IBAction func startTimer(_ sender: UIButton) {
         
-        guard let timerInfo = timeTextView.text else { return }
-        
-        do {
-            seconds = try timeToSeconds(for: timerInfo)
-        } catch {
-            print("Uh-oh!")
+        if hasStarted {
+            //pause timer
+            timer.invalidate()
+            sender.setTitle("START", for: .normal)
+            
+            hasStarted = false
+        } else {
+            //start timer
+            guard let timerInfo = timeTextView.text else {
+                Alerts.showBasicAlert(title: "Error", message: "There was some kind of error when trying to countdown", on: self)
+                return
+            }
+            
+            do {
+                seconds = try timeToSeconds(for: timerInfo)
+            } catch {
+                Alerts.showBasicAlert(title: "Invalid Time Format", message: "Please make sure you enter the time in the proper format of Hours:Minutes:Seconds. For example, 3 hours 15 minutes and 8 seconds should be entered as 03:15:08", on: self)
+            }
+            
+            guard seconds > 0 else {
+                Alerts.showBasicAlert(title: "Invalid Time Format", message: "Please enter the time in the proper format", on: self)
+                
+                return
+            }
+            
+            timeTextView.isEditable = false
+            resetButton.isHidden = false
+            
+            timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(count), userInfo: nil, repeats: true)
+            
+            sender.setTitle("PAUSE", for: .normal)
+            hasStarted = true
         }
         
-        guard seconds > 0 else { return }
-        
-        timeTextView.isEditable = false
-        resetButton.isHidden = false
-        
-        timer.invalidate()
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(count), userInfo: nil, repeats: true)
     }
     
     @IBAction func resetTimer(_ sender: UIButton) {
@@ -46,20 +68,27 @@ class TimerViewController: UIViewController {
     
     func reset() {
         timer.invalidate()
-                
-        seconds = 0
-        timeTextView.text = "00:00:00"
         resetButton.isHidden = true
+        
+        seconds = 0
+        
         timeTextView.isEditable = true
+        hasStarted = false
+       
+        startButton.setTitle("START", for: .normal)
+        timeTextView.text = "00:00:00"
+        
     }
     
     @objc func count() {
         seconds -= 1
         timeTextView.text = secondsToTime(for: seconds)
         
-        if timeTextView.text == "24:00:00"{
+        if timeTextView.text == "24:00:00" || timeTextView.text == "00:00:00"{
             reset()
+            Alerts.showBasicAlert(title: "Timer has finished!", message: "The time has finished counting down!", on: self)
         }
+        
     }
     
 }
